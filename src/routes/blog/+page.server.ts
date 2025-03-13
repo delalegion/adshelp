38b04eb41a4cf39548d6d844fcf402545a03b5a7
delalegion <nielotweb@gmail.com>
2025-03-13 12:06:51 +0100

    Fix deploy

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,5 +1,6 @@
 import type { PageServerLoad } from './$types';
-import { STRAPI_KEY, STRAPI_BASE } from "$env/static/private";
+import { STRAPI_KEY } from "$env/static/private";
+import { STRAPI_BASE } from "$env/static/private";
 
 export const load = (async () => {
     let response = await fetch(STRAPI_BASE + '/api/articles?populate=*', {
