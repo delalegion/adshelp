4c6def94dd4bed8fc333bd7be45353c75a6ce268
delalegion <nielotweb@gmail.com>
2025-05-06 13:20:34 +0200

    Add sentry

---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://bfdbabff339582e258d94e110a06323a@o4509275833499648.ingest.de.sentry.io/4509275837694032',
+
+  tracesSampleRate: 1.0,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: import.meta.env.DEV,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages(), tailwindcss()]
+	plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "adshelp",
+            project: "javascript-sveltekit"
+        }
+    }), sveltekit(), enhancedImages(), tailwindcss()]
 });
